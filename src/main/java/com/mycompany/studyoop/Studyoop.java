0123880e8e53816292566e5171ea4ca258d8cd9d
kenenthdevpc <kennethdevpc@gmail.com>
2023-05-31 17:55:01 -0500

    clase con array list

--- src/main/java/com/mycompany/studyoop/Studyoop.java
+++ src/main/java/com/mycompany/studyoop/Studyoop.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.studyoop;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author kenneth
@@ -12,14 +14,38 @@ public class Studyoop {
 
     public static void main(String[] args) {
         
-        Person persona = new Person();
-        persona.age=10;
-        persona.name="kenneth";
-        persona.height=2.3;
+        Person persona1 = new Person();
+        persona1.age=10;
+        persona1.name="kenneth";
+        persona1.height=2.3;
         
-        persona.ageMessage(); 
-        System.out.println(" en 15 años tendra"+persona.ageFuture(15)); 
+        persona1.ageMessage(); 
+        System.out.println(" en 15 años tendra: "+persona1.ageFuture(15)); 
         
+        Person persona2 = new Person();
+        persona2.age=20;
+        persona2.name="Diego";
+        persona2.height=1.3;
+        
+        
+        ArrayList<String>  lista = new ArrayList<String>();
+        lista.add("Kenneth");     
+        lista.add("Diego");   
+        lista.add("Juanes");
+        System.out.println("lista es: "+lista);
+        lista.remove("Kenneth");
+        System.out.println("lista es: "+lista);
+        lista.clear();
+        System.out.println("lista es: "+lista);
+        
+        ArrayList<Person>  lPersonas = new ArrayList<Person>();
+        lPersonas.add(persona1);    
+        lPersonas.add(persona2);
+        System.out.println(lPersonas.get(0).name);
+      
+        
+
+
 
         
     }
